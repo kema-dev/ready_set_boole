6726a70219d6899da414f2c8708572000fe03303
Kema-dev <01sludge_coaches@icloud.com>
2022-04-03 19:54:37 +0200

    added RPN eval

--- cmd/main.go
+++ cmd/main.go
@@ -11,5 +11,15 @@ func main() {
 	fmt.Printf("add(%d, %d) = %d\n", a, b, operation.Add(a, b))
 	fmt.Printf("substract(%d, %d) = %d\n", a, b, operation.Substract(a, b))
 	fmt.Printf("multiply(%d, %d) = %d\n", a, b, operation.Multiply(a, b))
-	fmt.Printf("GrayCode(%d) = %d\n", a, operation.GrayCode(a))
+	fmt.Printf("graycode(%d) = %d\n", a, operation.GrayCode(a))
+	s := "10&"
+	fmt.Printf("boolevel(%s) = %t\n", s, operation.BoolEval(s))
+	s = "10|"
+	fmt.Printf("boolevel(%s) = %t\n", s, operation.BoolEval(s))
+	s = "11>"
+	fmt.Printf("boolevel(%s) = %t\n", s, operation.BoolEval(s))
+	s = "10="
+	fmt.Printf("boolevel(%s) = %t\n", s, operation.BoolEval(s))
+	s = "1011||="
+	fmt.Printf("boolevel(%s) = %t\n", s, operation.BoolEval(s))
 }
--- pkg/operations.go
+++ pkg/operations.go
@@ -1,5 +1,7 @@
 package operation
 
+import "fmt"
+
 func Add(a int32, b int32) int32 {
 	sum := a ^ b
 	carry := (a & b) << 1
@@ -26,4 +28,80 @@ func Multiply(a int32, b int32) int32 {
 
 func GrayCode(a int32) int32 {
 	return a ^ (a >> 1)
+}
+
+func BoolEval(s string) bool {
+	// evaluate a boolean expression in RPN
+	if len(s) == 0 {
+		return false
+	}
+	stack := make([]bool, 0)
+	for _, c := range s {
+		switch c {
+		case '0':
+			stack = append(stack, false)
+		case '1':
+			stack = append(stack, true)
+		case '!':
+		if len(stack) < 1 {
+			return false
+		}
+		b1 := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		stack = append(stack, !b1)
+		case '&':
+			if len(stack) < 2 {
+				return false
+			}
+			b1 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			b2 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			stack = append(stack, b1 && b2)
+		case '|':
+			if len(stack) < 2 {
+				return false
+			}
+			b1 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			b2 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			stack = append(stack, b1 || b2)
+		case '^':
+			if len(stack) < 2 {
+				return false
+			}
+			b1 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			b2 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			stack = append(stack, b1 != b2)
+		case '>':
+			if len(stack) < 2 {
+				return false
+			}
+			b1 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			b2 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			stack = append(stack, !(b1 && !b2))
+		case '=':
+			if len(stack) < 2 {
+				return false
+			}
+			b1 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			b2 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			stack = append(stack, (b1 && b2) || (!b1 && !b2))
+		default:
+			fmt.Println("invalid character: ", string(c))
+			return false
+		}
+	}
+	if len(stack) != 1 {
+		fmt.Println("stack length is not 1 (", len(stack), ") stack:", stack)
+		return false
+	}
+	return stack[0]
 }
